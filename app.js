e97298a57c5076811bd732e1664ecc73812bfa99
Isaías González <ijgggr@gmail.com>
2025-12-02 22:57:15 -0800

    menu js

---
+++ app.js
@@ -0,0 +1,44 @@
+
+const baseIDs = [
+  "popular-items",
+  "burgers-and-sandwiches",
+  "lunch-specials",
+  "custom-combo",
+  "soups-and-salads",
+  "house-specials",
+  "dinner",
+  "combinations",
+  "extras-and-sides",
+  "beverages",
+  "beer-and-wine",
+  "breakfast",
+  "desserts",
+];
+
+// reset all tab styling and category visibility then select one ("example" -> "btn-example" & "div-example")
+function select(id) {
+  console.log("select", id);
+
+  // reset all btn
+  for (let i = 0; i < baseIDs.length; i++) {
+    document.getElementById("btn-" + baseIDs[i]).classList.remove("btn-menu-select");
+  }
+
+  // reset all div
+  for (let i = 0; i < baseIDs.length; i++) {
+    document.getElementById("div-" + baseIDs[i]).classList.remove("div-menu-select");
+  }
+
+  document.getElementById("btn-" + id).classList.add("btn-menu-select");
+  document.getElementById("div-" + id).classList.add("div-menu-select");
+}
+
+// SELECT POPULAR ITEMS
+select(baseIDs[0]);
+
+// CREATE BUTTON LISTENERS
+for (let i = 0; i < baseIDs.length; i++) {
+  document.getElementById("btn-" + baseIDs[i]).addEventListener("click", () => {
+    select(baseIDs[i]);
+  });
+}
