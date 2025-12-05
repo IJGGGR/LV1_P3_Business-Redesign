c1fcbcbe6c638de5475e348906151f1aaa544050
Isaías González <ijgggr@gmail.com>
2025-12-05 08:20:24 -0800

    menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,5 +1,8 @@
 
-const baseIDs = [
+// todo: do none of the numbered combos have sides listed? rice, beans, salad, and tortillas?
+// todo: combos > no. 4 : what kind of meat?
+
+const categoryIDs = [
   "popular-items",
   "burgers-and-sandwiches",
   "lunch-specials",
@@ -20,25 +23,61 @@ function select(id) {
   console.log("select", id);
 
   // reset all btn
-  for (let i = 0; i < baseIDs.length; i++) {
-    document.getElementById("btn-" + baseIDs[i]).classList.remove("btn-menu-select");
+  for (let i = 0; i < categoryIDs.length; i++) {
+    document.getElementById("btn-" + categoryIDs[i]).classList.remove("btn-menu-select");
   }
 
   // reset all div
-  for (let i = 0; i < baseIDs.length; i++) {
-    document.getElementById("div-" + baseIDs[i]).classList.remove("div-menu-select");
+  for (let i = 0; i < categoryIDs.length; i++) {
+    document.getElementById("div-" + categoryIDs[i]).classList.remove("div-menu-select");
   }
 
   document.getElementById("btn-" + id).classList.add("btn-menu-select");
   document.getElementById("div-" + id).classList.add("div-menu-select");
 }
 
+// POPULATE MENU ITEMS
+fetch("/data/menu.json")
+  .then(some => some.json())
+  .then(data => {
+    for (let i = 0; i < categoryIDs.length; i++) {
+      const curr = categoryIDs[i];
+      const list = data[curr]["list"];
+      const elem = document.querySelector(`#div-${curr} #list`);
+
+      elem.innerHTML = ""; // clear div
+
+      for (let j = 0; j < list.length; j++) {
+        const item = list[j];
+
+        // luckily empty elements don't render
+        elem.innerHTML +=
+          `<div class="row align-items-center">` +
+            `<div class="col-12 col-md">` +
+              `<span class="mb-0 h3 lh-sm">${item["name"]}</span>` +
+            `</div>` +
+            `<div class="col-12 col-md-auto">` +
+              `<span class="mb-0 h6 lh-sm">${item["cost"]}</span>` +
+            `</div>` +
+            `<div class="col-12">` +
+              `<span class="mb-0 fs-6 lh-base">${item["desc"] ?? ""}</span>` +
+            `</div>` +
+          `</div>`;
+
+        // avoids placing a trailing <hr>
+        if (j !== list.length - 1) {
+          elem.innerHTML += "<hr>";
+        }
+      }
+    }
+  });
+
 // SELECT POPULAR ITEMS
-select(baseIDs[0]);
+select(categoryIDs[0]);
 
 // CREATE BUTTON LISTENERS
-for (let i = 0; i < baseIDs.length; i++) {
-  document.getElementById("btn-" + baseIDs[i]).addEventListener("click", () => {
-    select(baseIDs[i]);
+for (let i = 0; i < categoryIDs.length; i++) {
+  document.getElementById("btn-" + categoryIDs[i]).addEventListener("click", () => {
+    select(categoryIDs[i]);
   });
 }
